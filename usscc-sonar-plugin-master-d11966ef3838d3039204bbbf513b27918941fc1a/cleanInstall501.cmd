694a26a241e50a38102616b39d3111849f7efa76
ashish <ashish.chouhan@impetus.co.in>
2017-04-24 12:38:47 +0530

    update usscc sonar Plugin tool

---
+++ usscc-sonar-plugin-master-d11966ef3838d3039204bbbf513b27918941fc1a/cleanInstall501.cmd
@@ -0,0 +1,3 @@
+copy .\target\sonar-usscc-plugin-0.1-SNAPSHOT.jar D:\softwares\sonarqube-5.0.1\extensions\downloads
+del D:\softwares\sonarqube-5.0.1\extensions\plugins\sonar-usscc-plugin-0.1-SNAPSHOT.jar
+D:\softwares\sonarqube-5.0.1\bin\windows-x86-64\StartSonar.bat
---
+++ usscc-sonar-plugin-master-d11966ef3838d3039204bbbf513b27918941fc1a/src/main/java/com/codecoverage/model/LogEntry.java
@@ -0,0 +1,106 @@
+package com.codecoverage.model;
+
+import java.util.List;
+
+/**
+ * The Class LogEntry is used for SVN to represent a SVN log entry.
+ */
+public class LogEntry {
+    
+    /** The author. */
+    private String author;
+    
+    /** The revision. */
+    private String revision;
+    
+    /** The msg. */
+    private String msg;
+    
+    
+    /** The paths. */
+    @SuppressWarnings("rawtypes")
+    List paths;
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "LogEntry [author=" + author + ", revision=" + revision + ", msg=" + msg + ", paths=" + paths + "]";
+    }
+
+    /**
+     *This method will  Gets the author and return the String type of value.
+     *
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * This method will  Sets the author and it will take a one parameter in type of String.
+     *
+     * @param author the new author
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     *This method will  Gets the revision and return the String type of value.
+     *
+     * @return the revision
+     */
+    public String getRevision() {
+        return revision;
+    }
+
+    /**
+     * This method will  Sets the revision and it will take a one parameter in type of String.
+     *
+     * @param revision the new revision
+     */
+    public void setRevision(String revision) {
+        this.revision = revision;
+    }
+
+    /**
+     *  this method will  Gets the msg and return the String type of value.
+     *
+     * @return the msg
+     */
+    public String getMsg() {
+        return msg;
+    }
+
+    /**
+     * this method will  Sets the msg and it will take a one parameter in type of String.
+     *
+     * @param msg the new msg
+     */
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    /**
+     * This method will  Gets the paths and return the List type of value.
+     *
+     * @return the paths
+     */
+    @SuppressWarnings("rawtypes")
+    public List getPaths() {
+        return paths;
+    }
+
+    /**
+     * This method will  Sets the paths and it will take a one parameter in type of List.
+     *
+     * @param paths the new paths
+     */
+    @SuppressWarnings("rawtypes")
+    public void setPaths(List paths) {
+        this.paths = paths;
+    }
+
+}
